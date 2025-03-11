287528cde7c450a12360837eca485e763aa8acad
root <root@ThatsFine>
2025-03-11 08:55:42 +0800

    20250311

--- src/main/java/org/example/searchenginedemo/mapper/DictMapper.java
+++ src/main/java/org/example/searchenginedemo/mapper/DictMapper.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -11,5 +12,6 @@ import java.util.Map;
 @Mapper
 public interface DictMapper {
     // 批量更新方法
+    @Transactional
     void batchUpdateDict(List<Map<String, String>> paramsList);
 }
